44e7fc0b2e683b73997049e9c6a245fb50a0d3ab
Lukxsx <33071953+Lukxsx@users.noreply.github.com>
2022-03-26 12:15:00 +0200

    Stripping whitespace, improving comments

--- src/main.c
+++ src/main.c
@@ -8,31 +8,31 @@
 
 
 int main(int argc, char **argv) {
-	// Process the command line options
-	int opt, ai_mode = 0;
-	while ((opt = getopt(argc, argv, "uah")) != -1) {
-		switch (opt) {
-			case 'a':
-				ai_mode = 1;
-				break;
-			case 'h':
-				printusage(argv[0]);
-				exit(EXIT_SUCCESS);
-			case '?':
-				printf("Unknown option: %c\n", optopt);
-				printusage(argv[0]);
-				exit(EXIT_FAILURE);
-		}
-	}
-	
-	if (ai_mode) {
-		printf("AI mode enabled\n");
-		
-	} else {
-		play();
-	}
-	
-	
-	
-	exit(EXIT_SUCCESS);
+    // Process the command line options
+    int opt, ai_mode = 0;
+    while ((opt = getopt(argc, argv, "uah")) != -1) {
+        switch (opt) {
+            case 'a':
+                ai_mode = 1;
+                break;
+            case 'h':
+                printusage(argv[0]);
+                exit(EXIT_SUCCESS);
+            case '?':
+                printf("Unknown option: %c\n", optopt);
+                printusage(argv[0]);
+                exit(EXIT_FAILURE);
+        }
+    }
+
+    if (ai_mode) { // start in AI mode
+        printf("AI mode enabled\n");
+
+    } else {
+        play(); // play the game normally
+    }
+
+
+
+    exit(EXIT_SUCCESS);
 }
